e3bd8908b6c75539842e30e9c40512161580813d
bonsa <bonsa>
2024-06-04 23:27:44 +0300

    first_success

--- react_router/src/main.jsx
+++ react_router/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Profile from "./profile.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Corrected prop name
+  },
+  {
+    path: "/profile",
+    element: <Profile />, // Corrected prop name
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ react_router/src/profile.jsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <div>
+      <h1>Hello from profile page!</h1>
+      <p>So, how are you?</p>
+    </div>
+  );
+};
+
+export default Profile;
